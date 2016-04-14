048ac9140b97ab566727618d35d8f97cb8e22aff
agate <agate.hao@gmail.com>
2016-04-14 16:46:56 +0800

    Add init migration

--- server/README.md
+++ server/README.md
@@ -14,6 +14,13 @@ To start a web server for the application, run:
 
     lein ring server
 
+## MIGRATION
+
+Migration files located at $ROOT/resources/migrations
+
+    lein migratus migrate
+    lein migratus rollback
+
 ## License
 
 Copyright © 2016 FIXME
--- server/project.clj
+++ server/project.clj
@@ -1,12 +1,22 @@
 (defproject . "0.1.0-SNAPSHOT"
-  :description "Factual Kudo System"
-  :url "https://github.com/Factual/kyoodos"
-  :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [compojure "1.5.0"]
-                 [ring/ring-defaults "0.1.5"]]
-  :plugins [[lein-ring "0.9.7"]]
-  :ring {:handler com.factual.kyoodos.handler/app}
-  :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]]}})
+	:description "Factual Kudo System"
+	:url "https://github.com/Factual/kyoodos"
+	:min-lein-version "2.0.0"
+	:dependencies [[org.clojure/clojure "1.8.0"]
+								 [compojure "1.5.0"]
+								 [ring/ring-defaults "0.1.5"]
+                 [org.slf4j/slf4j-log4j12 "1.7.9"]
+                 [postgresql "9.3-1102.jdbc41"]
+                 [migratus "0.8.13"]]
+	:plugins [[lein-ring "0.9.7"]
+						[migratus-lein "0.2.6"]]
+	:ring {:handler com.factual.kyoodos.handler/app}
+	:profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+																	[ring/ring-mock "0.3.0"]]}}
+	:migratus {:store :database
+						 :migration-dir "migrations"
+						 :db {:classname "org.postgresql.Driver"
+									:subprotocol "postgresql"
+									:subname "//localhost:5432/kyoodos"
+									:user "postgres"
+									:password "postgres"}})
