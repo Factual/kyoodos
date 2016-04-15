f475edaaa126e79e2c72ce9516b45f9dd1b27e49
sarahbkim <sarahkim@factual.com>
2016-04-15 11:54:32 -0700

    stub some basic routes for api, along with tests

--- server/src/com/factual/kyoodos/handler.clj
+++ server/src/com/factual/kyoodos/handler.clj
@@ -3,9 +3,36 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
+(defn post-kyoodos [request]
+  (let [content (get-in request [:params :content])]
+    (str "You posted: " content)))
+    ;{:status 200 :headers {"Content-type" "application/json"} :body "{}"})
+
+; if no :user_or_group_id, get current_user kyoodos
+(defn get-kyoodos [request]
+  (let [user-group-id (get-in request [:params :user_or_group_id])]
+    (let [direction (get-in request [:params :direction])]
+      (str "You requested kyoodos " direction " "  user-group-id))))
+    ;{:status 200 :headers {"Content-type" "application/json"} :body "{}"})))
+  
+(defn get-user [request]
+  (let [user-id (get-in request [:params :user_id])]
+    (str "You requested user: " user-id)))
+
+(defn get-group [request]
+  (let [group-id (get-in request [:params :group_id])]
+    (str "You requested user: " group-id)))
+
 (defroutes app-routes
   (GET "/" [] "Hello World")
+  (GET "/test" [] 
+    {:status 200 :headers {"Content-type" "application/json"} :body "{}"})
+  (GET "/kyoodos" [] get-kyoodos)
+  (POST "/kyoodos" [] post-kyoodos)
+  (GET "/user" [] get-user)
+  (GET "/group" [] get-group)
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  ; SK - turning off CSRF protection temporarily... 
+  (wrap-defaults app-routes (assoc-in site-defaults [:security :anti-forgery] false) ))
--- server/test/com/factual/kyoodos/handler_test.clj
+++ server/test/com/factual/kyoodos/handler_test.clj
@@ -12,3 +12,23 @@
   (testing "not-found route"
     (let [response (app (mock/request :get "/invalid"))]
       (is (= (:status response) 404)))))
+
+  (testing "get kyoodos route"
+    (let [response (app (mock/request :get "/kyoodos" {:user_or_group_id "1", :direction "from"}))]
+      (is (= (:status response) 200))
+      (is (= (:body response) "You requested kyoodos from 1"))))
+
+  (testing "post kyoodos route"
+    (let [response (app (mock/request :post "/kyoodos" {:content "good job"}))]
+      (is (= (:status response) 200))
+      (is (= (:body response) "You posted: good job"))))
+
+  (testing "get user route"
+    (let [response (app (mock/request :get "/user" {:user_id "1"}))]
+      (is (= (:status response) 200))
+      (is (= (:body response) "You requested user: 1"))))
+
+  (testing "get group route"
+    (let [response (app (mock/request :get "/group" {:group_id "1"}))]
+      (is (= (:status response) 200))
+      (is (= (:body response) "You requested group: 1"))))
