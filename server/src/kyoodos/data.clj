b56c86eaf1f76c98e66b147392786b48fee40d08
agate <agate.hao@gmail.com>
2016-06-07 21:05:23 +0800

    fix syntax error

--- server/src/kyoodos/data.clj
+++ server/src/kyoodos/data.clj
@@ -3,7 +3,6 @@
   (:use [kyoodos.utils.query_generator]))
 
 (defqueries "queries/queries.sql")
-
   
 (defn get-user [user-id]
   (get-user-raw {:user_id user-id}))
@@ -15,7 +14,7 @@
   (case (str direction)
         "to" (get-to-kyoodos-raw {:user_or_group_id id})
         "from" (get-from-kyoodos-raw {:user_or_group_id id})
-        (get-kyoodos-raw))
+        (get-kyoodos-raw)))
 
 (defn valid-user? [id]
   (let [existence (valid-user {:id id})]
@@ -28,4 +27,3 @@
 (defn insert-kyoodos [content]
   (let [to (get-in content [:to])]
     (if (or (valid-user? to) (valid-group? to)) (insert-kyoodo-raw! {:from_user_id (content :from) :to_user_id (content :to) :content_raw (generate-string content) :content (content :content)}) (str "No such user or group: " to))))
-
