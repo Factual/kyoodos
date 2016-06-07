bb306ab2a1e528ace815a24c4af3f07024dfd990
sarahbkim <sarahkim@factual.com>
2016-06-07 16:07:11 -0700

    add avatar col

--- server/models/db.js
+++ server/models/db.js
@@ -6,6 +6,6 @@ var client = new pg.Client(connectionString);
 client.connect();
 
 // update existing pg tables from clj project 
-var query = client.query('DROP TABLE IF EXISTS groups; DROP TABLE IF EXISTS user_groups; CREATE TABLE IF NOT EXISTS slack_users(id VARCHAR(40) not null, first_name VARCHAR(40), last_name VARCHAR(80), email VARCHAR(100))');
+var query = client.query('DROP TABLE IF EXISTS groups; DROP TABLE IF EXISTS user_groups; CREATE TABLE IF NOT EXISTS slack_users(id VARCHAR(40) not null, first_name VARCHAR(40), last_name VARCHAR(80), email VARCHAR(100)); ALTER TABLE users ADD COLUMN avatar VARCHAR(100)');
 query.on('end', function() { client.end(); });
 
