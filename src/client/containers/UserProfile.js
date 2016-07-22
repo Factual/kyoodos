a233eb07e4c4ef2959d48b8c0a5e05673a92d558
sarahbkim <sarahkim@factual.com>
2016-07-22 15:59:54 -0700

    fix users:user page

--- src/client/containers/UserProfile.js
+++ src/client/containers/UserProfile.js
@@ -5,47 +5,69 @@ import { getFromKyoodos, getToKyoodos, getLastSent, getLastReceived } from '../a
 import { getUsersFromKyoodo } from '../utils'
 
 let UserProfile = React.createClass({
+  getInitialState: function() {
+    return {
+      data_from: [],
+      data_to: []
+    }
+  },
   componentWillMount: function() {
-    console.log('will');
     this.props.getFromKyoodos(this.props.params.userId);
     this.props.getToKyoodos(this.props.params.userId);
   },
+  componentWillReceiveProps: function(newProps) {
+    this.setState({ data_from: [...this.state.data_from, ...newProps.from] })
+    this.setState({ data_to: [...this.state.data_to, ...newProps.to] })
+  },
   render: function() {
 
-    if (this.props.from || this.props.to) {
-      let kyoodos = {
-            from: [],
-            to: []
-      };
+    if (this.state.data_from || this.state.data_to) {
+      let from = [],
+          to = [];
+
+      let users, to_users, receivers;
 
       // for both to and from kyoodos ...
-
-      Object.keys(kyoodos).forEach((key, i) => {
-        (this.props[key]).forEach((k) => {
-          let users = getUsersFromKyoodo(k)
-          let to_users = users.filter((u) => { return u != k.from_user_id })
-          let receivers = to_users.map((curr) => {
-            return this.props.users[curr] || {}
-          })
-          kyoodos[key].push(
-            <Kyoodo key = { k.id + i }
-                    from_user = { this.props.users[k.from_user_id] || 'unknown' }
-                    receivers = { receivers }
-                    users = { this.props.users }
-                    content = { k.content } />
-          )
+      this.state.data_from.forEach((k, i) => {
+        users = getUsersFromKyoodo(k)
+        to_users = users.filter((u) => { return u != k.from_user_id })
+        receivers = to_users.map((curr) => {
+          return this.props.users[curr] || {}
         })
+
+        from.push(
+          <Kyoodo key={ 'from' + k.id + i + 'from' }
+                  from_user = { this.props.users[k.from_user_id] || 'unknown' }
+                  receivers = { receivers }
+                  users = { this.props.users }
+                  content = { k.content } />
+        )
+      })
+
+      this.state.data_to.forEach((k, i) => {
+        users = getUsersFromKyoodo(k)
+        to_users = users.filter((u) => { return u != k.from_user_id })
+        receivers = to_users.map((curr) => {
+          return this.props.users[curr] || {}
+        })
+        to.push(
+          <Kyoodo key={ 'to' + k.id + i + 'to' }
+                  from_user = { this.props.users[k.from_user_id] || 'unknown' }
+                  receivers = { receivers }
+                  users = { this.props.users }
+                  content = { k.content } />
+        )
       })
 
       return (
         <div className='row medium-unstack kyoodos'>
           <div className='medium-6 columns'>
             <h3>kyoodos sent</h3>
-              { kyoodos.from }
-            </div>
+            { from }
+          </div>
           <div className='medium-6 columns'>
             <h3>kyoodos received</h3>
-            { kyoodos.to }
+            { to }
           </div>
         </div>
       )
